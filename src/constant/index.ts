6edee5e9c1a5f3786a7cc64bd5f0d064515a0a79
Xinglee23 <xinglee23@outlook.com>
2023-04-03 10:38:10 +0800

    Update index.ts

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,4 +1,3 @@
-export const OPENAI_API_KEY =
-  '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';
+export const OPENAI_API_KEY = '';
 
-export const ORG_ID = 'org-egqnRzvIbkdeOImDH37HqAS8';
+export const ORG_ID = '';
